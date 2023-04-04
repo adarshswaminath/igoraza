c77ffde38c17be73b1fc95a027e3e0357fe6b0c2
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-04 17:59:30 +0530

    Hero section update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div>
       <section className="text-gray-700 bg-gray-100 mb-3">
-        <div className="hero h-screen bg-cover bg-no-repeat bg-center relative" style={{ backgroundImage: `url(${image})` }}>
+        <div className="hero h-screen bg-cover bg-no-repeat bg-center relative">
           <div className="bg-gradient-to-b from-transparent to-gray-900 h-full w-full absolute"></div>
           <div className="hero-content flex-col lg:flex-row-reverse h-full">
             <div className="flex flex-col justify-center px-4 lg:px-0 lg:ml-8 xl:ml-16 2xl:ml-24">
@@ -15,9 +15,9 @@ function Hero() {
                 <p>Start with something simple and small,</p>
                 <p>then expand over time</p>
               </p>
-              <button className="btn w-40 lg:w-96 mx-auto border-none rounded-full bg-[#1d3557] hover:bg-[#1d3545] text-white font-bold uppercase tracking-wider">
+              <a className="btn w-40 lg:w-96 mx-auto border-none rounded-full bg-[#1d3557] hover:bg-[#1d3545] text-white font-bold uppercase tracking-wider">
                 Get Started
-              </button>
+              </a>
             </div>
           </div>
         </div>
