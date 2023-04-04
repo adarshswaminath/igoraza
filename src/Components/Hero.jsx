f918834e1c5476fe78141f1fc1f65f722abd056a
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-04 17:52:34 +0530

    Hero section update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,43 +3,24 @@ import image from "./images/hero.svg"
 function Hero() {
   return (
     <div>
-      <section className="text-gray-700 bg-gray-100">
-      <section className="h-screen">
-            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-              <div className="mr-auto place-self-center lg:col-span-7">
-                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Welcome To IGORAZA</h1>
-                <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">
+      <section className="text-gray-700 bg-gray-100 mb-3">
+        <div className="hero h-screen bg-cover bg-no-repeat bg-center relative" style={{ backgroundImage: `url(${image})` }}>
+          <div className="bg-gradient-to-b from-transparent to-gray-900 h-full w-full absolute"></div>
+          <div className="hero-content flex-col lg:flex-row-reverse h-full">
+            <div className="flex flex-col justify-center px-4 lg:px-0 lg:ml-8 xl:ml-16 2xl:ml-24">
+              <h1 className="text-5xl lg:text-6xl font-sans font-extrabold text-white leading-tight">
+                Welcome To IGORAZA
+              </h1>
+              <p className="py-6 text-lg text-gray-200">
                 <p>Start with something simple and small,</p>
-              <p>then expand over time</p>
-                </p>
-              </div>
-              <div className="lg:mt-0 lg:col-span-5 lg:flex">
-                <img src={image} alt="mockup" />
-              </div>
-            </div>
-          </section>
-        {/* <div className="container h-screen flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-          <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className="text-2xl mt-14 sm:text-6xl  leading-56 uppercase font-semibold">
-              WELCOME TO IGORAZA
-            </h1>
-            <div className="mt-3 mb-8 sm:mb-12">
-              <p>Start with something simple and small,</p>
-              <p>then expand over time</p>
-            </div>
-            <div className="">
-              <a href="#view" 
-              className="px-12 py-3 border-2 border-gray-500 text-black hover:bg-[#1d3557] hover:text-white hover:border-none font-bold">Get Started</a>
+                <p>then expand over time</p>
+              </p>
+              <button className="btn w-40 lg:w-96 mx-auto border-none rounded-full bg-[#1d3557] hover:bg-[#1d3545] text-white font-bold uppercase tracking-wider">
+                Get Started
+              </button>
             </div>
           </div>
-          <div className="flex items-center justify-center p-6 mt-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-            <img
-              src={image}
-              alt=""
-              className="object-contain h-72 sm:h-82 lg:h-96 xl:h-112 2xl:h-128"
-            />
-          </div>
-        </div> */}
+        </div>
       </section>
     </div>
   );
