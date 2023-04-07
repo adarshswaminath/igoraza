7c36ad8b8f1076e5d78306bcb716d9d63cd77119
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-07 12:10:33 +0530

    images update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import industry from "./images/industry.svg"
+import product1 from "./images/product1.jpg"
+import product2 from "./images/product2.jpg"
+import product3 from "./images/product3.jpg"
 function Projects() {
   return (
     <div>
@@ -12,7 +15,7 @@ function Projects() {
                 <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">Igoraza Industrials is a dynamic and innovative startup that offers a range of workshops and programs to help young professionals and entrepreneurs reach their full potential. Our programs are designed to provide participants with the skills, knowledge, and experience they need to succeed in their careers and achieve their goals.</p>
               </div>
               <div className="lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+                <img src={industry} alt="mockup" />
               </div>
             </div>
           </section>
@@ -44,21 +47,21 @@ function Projects() {
             <div className="grid md:grid-cols-3 gap-3 justify-center items-center lg:flex">
               {/* <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:flex justify-center mx-auto"> */}
               <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-56 object-cover object-center" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Collab Workshops" />
+                <img className="w-full h-56 object-cover object-center" src={product3} alt="Collab Workshops" />
                 <div className="px-4 py-2">
                   <h2 className="text-gray-800 font-bold text-2xl mb-2">Collab Workshops</h2>
                   <p className="text-gray-600 text-sm">Our collab workshops bring together like-minded individuals who are passionate about entrepreneurship, innovation, and collaboration. Participants will work on real-world projects, learn from industry experts, and develop the skills they need to succeed in today's competitive business environment.</p>
                 </div>
               </div>
               <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden mt-6 md:mt-0">
-                <img className="w-full h-56 object-cover object-center" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Incubation Programme" />
+                <img className="w-full h-56 object-cover object-center" src={product2} alt="Incubation Programme" />
                 <div className="px-4 py-2">
                   <h2 className="text-gray-800 font-bold text-2xl mb-2">Incubation Programme</h2>
                   <p className="text-gray-600 text-sm">Our incubation programmes offer aspiring business owners the assistance and tools they need to start their own companies. Mentorship, training, funding, and access to other resources will be provided to participants in order to assist them in transforming their ideas into profitable businesses.</p>
                 </div>
               </div>
               <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden mt-6 md:mt-0">
-                <img className="w-full h-56 object-cover object-center" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Internship Programs" />
+                <img className="w-full h-56 object-cover object-center" src={product1} alt="Internship Programs" />
                 <div className="px-4 py-2">
                   <h2 className="text-gray-800 font-bold text-2xl mb-2">Internship Programs</h2>
                   <p className="text-gray-600 text-sm ">Our internship programs offer mentorship opportunities and networking events to help participants grow their professional network and gain insights into the industry. We believe in investing in the next generation of talent and providing them with the resources and support they need to reach their full potential</p>
