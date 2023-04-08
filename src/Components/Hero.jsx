2407c6f0427f621e079bbfac070f06055b14b966
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-08 19:46:54 +0530

    Images changed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import image from "./images/launch.png"
+import heroimage from "./images/background.jpg"
 function Hero() {
   return (
     <div>
-      <section className="text-gray-700 mb-3">
-        <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/bQdghKV/background.jpg")` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="">
+            <h1 className="text-5xl font-raleway font-extrabold">WELCOME TO IGORAZA</h1>
+            <p className="py-6 text-lg">
+              <p>Start with something simple and small,</p>
+              <p>then expand over time</p>
+            </p>
+            <a href="#about" className="btn w-40 lg:w-96 mx-auto border-none rounded-full bg-[#1d3557] hover:bg-[#1d3545] text-white font-bold uppercase tracking-wider">
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <section className="text-gray-700 mb-3">
+      <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero min-h-screen " style={{ backgroundImage: `url("https://i.ibb.co/bQdghKV/background.jpg")` }}>
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img src={image} className="max-w-sm" />
             <div>
@@ -19,24 +36,7 @@ function Hero() {
             </div>
           </div>
         </div>
-        {/* <div className="hero h-screen bg-cover bg-no-repeat bg-center relative" style={{ backgroundImage: `url("https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwNzItMDAzLnBuZw.png")` }}>
-          <div className="bg-gradient-to-b from-transparent to-gray-900 h-full w-full absolute"></div>
-          <div className="hero-content flex-col lg:flex-row-reverse h-full">
-            <div className="flex flex-col justify-center px-4 lg:px-0 lg:ml-8 xl:ml-16 2xl:ml-24">
-              <h1 className="text-5xl lg:text-6xl font-sans font-extrabold text-white leading-tight">
-                Welcome To IGORAZA
-              </h1>
-              <p className="py-6 text-lg text-gray-200">
-                <p>Start with something simple and small,</p>
-                <p>then expand over time</p>
-              </p>
-              <a href="#about" className="btn w-40 lg:w-96 mx-auto border-none rounded-full bg-[#1d3557] hover:bg-[#1d3545] text-white font-bold uppercase tracking-wider">
-                Get Started
-              </a>
-            </div>
-          </div>
-        </div> */}
-      </section>
+      </section> */}
     </div>
   );
 }
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -2,16 +2,31 @@ import { SiHiveBlockchain } from "react-icons/si"
 import { CgWebsite } from "react-icons/cg"
 import { MdOutlineDeveloperMode, MdOutlineConstruction } from 'react-icons/md'
 import MissionImg from './images/service.svg';
+import bgimage from "./images/service.png"
 
 
 function Mission() {
     return (
         <div>
-            <div className="hero min-h-screen bg-white text-gray-600" >
+            {/* <section class="" style={{ backgroundImage: `url("https://images.pexels.com/photos/433154/pexels-photo-433154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
+                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                    <div class="mr-auto place-self-center lg:col-span-7">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-4xl font-bold text-gray-600">IGORAZA Consultancy</h1>
+                            <p className="mb-5 text-gray-600">We help businesses propel into the digital service economy with our innovative solutions and advisory services. </p>
+                        </div>
+                    </div>
+                    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                        <img src={bgimage} alt="mockup" />
+                    </div>
+                </div>
+            </section> */}
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.pexels.com/photos/433154/pexels-photo-433154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-4xl font-bold text-gray-600">IGORAZA Consultancy</h1>
-                        <p className="mb-5 text-gray-600">We help businesses propel into the digital service economy with our innovative solutions and advisory services. </p>
+                        <h1 className="mb-5 text-4xl font-bold text-white">IGORAZA Consultancy</h1>
+                        <p className="mb-5 text-white">We help businesses propel into the digital service economy with our innovative solutions and advisory services. </p>
                     </div>
                 </div>
             </div>
@@ -44,7 +59,7 @@ function Mission() {
                                 <CgWebsite className="text-4xl mb-4" />
                                 <h2 className="text-xl font-bold mb-2">Website Design & Development</h2>
                                 <p className="text-lg">
-                                We create exceptional, secure, and high-performing custom websites for big businesses with engaging, professional designs. Your website is often the first impression customers have of your company, so it's crucial to have a visually appealing site. You can rely on us to deliver a website that performs well.</p>
+                                    We create exceptional, secure, and high-performing custom websites for big businesses with engaging, professional designs. Your website is often the first impression customers have of your company, so it's crucial to have a visually appealing site. You can rely on us to deliver a website that performs well.</p>
                             </div>
                         </div>
                     </div>
@@ -62,7 +77,7 @@ function Mission() {
                             <MdOutlineConstruction className="text-4xl mb-4" />
                             <h2 className="text-xl font-bold mb-2">Website Reconstruct</h2>
                             <p className="text-lg">
-                            Our objective is to provide your website with a fresh, elegant, and streamlined look, while staying ahead of the competition through innovative plugin updates, content refreshing, and seamless integration for improved user experience and functionality</p>
+                                Our objective is to provide your website with a fresh, elegant, and streamlined look, while staying ahead of the competition through innovative plugin updates, content refreshing, and seamless integration for improved user experience and functionality</p>
                         </div>
                     </div>
 
